4c083ab947f58cc4a3e406eae8b7589035c3caa8
Javier <ggutierrez.javier@gmail.com>
2020-07-25 18:47:05 -0400

    Calculadora!

---
+++ main.py
@@ -0,0 +1,31 @@
+import re
+print("Calculadora!")
+print("Escribe 'salir' para salir\n")
+
+previo = 0
+validador = True
+
+def realiza_calculo():
+    global validador
+    global previo
+    ecuacion = ""
+    if previo == 0:
+        ecuacion = input("Escribe tu ecuación: ")
+    else:
+        ecuacion = input(str(previo))
+
+
+    if ecuacion == 'salir':
+        print("Hasta la vista, humano.")
+        validador = False
+    else:
+        # Usamos RegEx para prevenir vulnerabilidades de la función eval().
+        ecuacion = re.sub('[a-zA-Z,.:()" "]','',ecuacion)
+
+        if previo == 0:
+            previo = eval(ecuacion)
+        else:
+            previo = eval(str(previo) + ecuacion)
+
+while validador:
+    realiza_calculo()
